2e22a53d3d56029441b84311ca0335dacabac800
KIMINOU Degrace <kiminoudegrace64@gmail.com>
2026-08-05 01:30:16 +0100

    Restore plugin ZIP generation during build

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,4 +1,5 @@
 import esbuild from "esbuild";
+import { execSync } from "child_process";
 
 esbuild.build({
   entryPoints: ["src/main.js"],
@@ -8,4 +9,12 @@ esbuild.build({
   platform: "browser",
   minify: true,
   sourcemap: false
+}).then(() => {
+  console.log("Build completed. Creating plugin ZIP...");
+
+  execSync("node pack-zip.js", {
+    stdio: "inherit"
+  });
+
+  console.log("plugin.zip created successfully.");
 }).catch(() => process.exit(1));
